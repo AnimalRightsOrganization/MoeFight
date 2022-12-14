87a70c3d0bab55d7ce45385d9aae024105d5549b
setsuodu <339131001@qq.com>
2022-12-14 17:59:41 +0800

    Timeline加载后绑定

--- Unity/Assets/Scripts/TimelineControl.cs
+++ Unity/Assets/Scripts/TimelineControl.cs
@@ -4,24 +4,63 @@ using UnityEngine.Playables;
 
 public class TimelineControl : MonoBehaviour
 {
-	public List<PlayableDirector> timelines;
+    public Animator honoka;
+
+    public List<PlayableDirector> timelines;
     public int currLine = 0;
 
+    public PlayableAsset asset_HADOUKEN;
+    public PlayableAsset asset_SHORYUKEN;
     public GameObject vcam_HADOUKEN;
     public GameObject vcam_SHORYUKEN;
-    public PlayableAsset HADOUKEN;
-    public PlayableAsset SHORYUKEN;
+
+    const string CinemachineTrackName = "Cinemachine Track";
+    const string AnimationTrackName = "Animation Track";
 
     void Awake()
     {
-        vcam_HADOUKEN = ResManager.LoadPrefab("Timeline/vcam_HADOUKEN");
-        vcam_SHORYUKEN = ResManager.LoadPrefab("Timeline/vcam_SHORYUKEN");
-    }
+        honoka = GetComponent<Animator>();
 
-    void Start()
-    {
-        HADOUKEN = ResManager.LoadTimeline("Timeline/HADOUKEN") as PlayableAsset;
-        SHORYUKEN = ResManager.LoadTimeline("Timeline/SHORYUKEN") as PlayableAsset;
+        asset_HADOUKEN = ResManager.LoadTimeline("Timeline/HADOUKEN") as PlayableAsset;
+        asset_SHORYUKEN = ResManager.LoadTimeline("Timeline/SHORYUKEN") as PlayableAsset;
+
+        vcam_HADOUKEN = ResManager.LoadPrefab("Timeline/vcam_HADOUKEN");
+        GameObject obj_HADOUKEN = Instantiate(vcam_HADOUKEN);
+        obj_HADOUKEN.name = "vcam_HADOUKEN";
+        PlayableDirector direct_HADOUKEN = obj_HADOUKEN.GetComponent<PlayableDirector>();
+        timelines.Add(direct_HADOUKEN);
+        direct_HADOUKEN.playableAsset = asset_HADOUKEN;
+        foreach (var playableAssetOutput in direct_HADOUKEN.playableAsset.outputs)
+        {
+            Debug.Log($"{playableAssetOutput.streamName}");
+            if (playableAssetOutput.streamName == AnimationTrackName)
+            {
+                direct_HADOUKEN.SetGenericBinding(playableAssetOutput.sourceObject, honoka.gameObject);
+            }
+            else if (playableAssetOutput.streamName == CinemachineTrackName)
+            {
+                direct_HADOUKEN.SetGenericBinding(playableAssetOutput.sourceObject, Camera.main.gameObject);
+            }
+        }
+
+        vcam_SHORYUKEN = ResManager.LoadPrefab("Timeline/vcam_SHORYUKEN");
+        GameObject obj_SHORYUKEN = Instantiate(vcam_SHORYUKEN);
+        obj_SHORYUKEN.name = "vcam_SHORYUKEN";
+        PlayableDirector direct_SHORYUKEN = obj_SHORYUKEN.GetComponent<PlayableDirector>();
+        timelines.Add(direct_SHORYUKEN);
+        direct_SHORYUKEN.playableAsset = asset_SHORYUKEN;
+        foreach (var playableAssetOutput in direct_SHORYUKEN.playableAsset.outputs)
+        {
+            Debug.Log($"{playableAssetOutput.streamName}");
+            if (playableAssetOutput.streamName == AnimationTrackName)
+            {
+                direct_SHORYUKEN.SetGenericBinding(playableAssetOutput.sourceObject, honoka.gameObject);
+            }
+            else if (playableAssetOutput.streamName == CinemachineTrackName)
+            {
+                direct_SHORYUKEN.SetGenericBinding(playableAssetOutput.sourceObject, Camera.main.gameObject);
+            }
+        }
     }
 
     void Update()
