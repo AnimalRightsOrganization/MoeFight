8438990df7f58159cf2700c24a69472c79121750
setsuodu <setsuodu@users.noreply.github.com>
2024-08-25 23:58:40 +0800

    db backup

---
+++ DB/db_app.sql
@@ -0,0 +1,38 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50736
+Source Host           : localhost:3306
+Source Database       : db_app
+
+Target Server Type    : MYSQL
+Target Server Version : 50736
+File Encoding         : 65001
+
+Date: 2024-08-25 23:53:23
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tb_app
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_app`;
+CREATE TABLE `tb_app` (
+  `product_name` varchar(255) DEFAULT NULL,
+  `web` varchar(255) DEFAULT NULL,
+  `gate` varchar(255) DEFAULT NULL,
+  `app_url` varchar(255) DEFAULT NULL,
+  `res_url` varchar(255) DEFAULT NULL,
+  `app_version` varchar(255) DEFAULT NULL,
+  `res_version` varchar(255) DEFAULT NULL,
+  `notice` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=gbk;
+
+-- ----------------------------
+-- Records of tb_app
+-- ----------------------------
+INSERT INTO `tb_app` VALUES ('moefight', 'moegijinka.cn', 'moegijinka.cn', 'moegijinka.cn', 'app.moegijinka.cn/moefight/res', '1.0', '1.0', '没有公告');
+INSERT INTO `tb_app` VALUES ('turtlerace', 'moegijinka.cn', 'moegijinka.cn', 'moegijinka.cn', 'app.moegijinka.cn/turtlerace/res', '1.0', '1.0', '没有公告');
+INSERT INTO `tb_app` VALUES ('afk', 'moegijinka.cn', 'moegijinka.cn', 'moegijinka.cn', 'app.moegijinka.cn/afk/res', '1.0', '1.0', '没有公告');
---
+++ DB/db_user.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50736
+Source Host           : localhost:3306
+Source Database       : db_user
+
+Target Server Type    : MYSQL
+Target Server Version : 50736
+File Encoding         : 65001
+
+Date: 2024-08-25 23:53:33
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
